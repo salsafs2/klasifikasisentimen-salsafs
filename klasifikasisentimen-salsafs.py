85ef4599961cfe3fc1ab0fe8855d592f7dc22287
salsafs2 <148967660+salsafs2@users.noreply.github.com>
2025-04-30 15:43:16 +0700

    Add files via upload

---
+++ klasifikasisentimen-salsafs.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from textblob import TextBlob
+
+# Daftar kata positif dan negatif
+positive_words = {'baik', 'bagus', 'lengkap', 'mantap', 'top', 'good', 'nice',
+                  'manfaat', 'bantu', 'mudah', 'rekomendasi', 'sederhana', 'menarik',
+                  'terimakasih', 'keren', 'puas', 'jelas', 'lancar', 'aman', 'nyaman',
+                  'untung', 'ada', 'tersedia', 'terdaftar', 'dapat', 'jos'}
+negative_words = {'lambat', 'jelek', 'aneh', 'heran', 'gagal', 'emosi', 'kecewa', 
+                  'ganggu', 'sebal', 'tidak ada', 'tidak bisa', 'tidak dapat', 
+                  'buruk', 'tidak jelas', 'tidak sesuai', 'freeze', 'sulit', 'lag', 
+                  'bug', 'keluar', 'rusak', 'ups', 'haduh', 'eror', 'terhambat', 
+                  'blokir', 'payah', 'hilang', 'tidak terdaftar', 'tidak tersedia', 
+                  'ribet', 'riweh', 'pusing', 'parah', 'bobrok', 'maintenance', 
+                  'kendala', 'masalah', 'down', 'buruk', 'dadak', 'lama', 'tai', 
+                  'anjing'}
+
+def detect_sentiment(text):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    if sentiment > 0:
+        return "Positif"
+    elif sentiment < 0:
+        return "Negatif"
+
+def detect_custom_sentiment(text):
+    words = text.lower().split()
+    positive_count = sum(1 for word in words if word in positive_words)
+    negative_count = sum(1 for word in words if word in negative_words)
+    if negative_count > positive_count:
+        return "Negatif"
+    elif positive_count > negative_count:
+        return "Positif"
+
+# Streamlit UI
+st.title("Sentiment Analysis")
+
+# Membatasi ukuran kolom teks dengan CSS agar lebih sempit
+st.markdown("""
+    <style>
+    .stTextArea textarea {
+        width: 300px !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+text = st.text_input("Input Text:")
+sentiment =''
+
+if st.button('Analyze Sentiment'):
+    if text:
+        sentiment = detect_sentiment(text)
+        custom_sentiment = detect_custom_sentiment(text)
+
+        # Menentukan warna berdasarkan sentimen
+        if custom_sentiment == "Negatif":
+            st.markdown(f"Sentimen Ulasan: <span style='color:red; font-weight:bold;'>{custom_sentiment}</span>", unsafe_allow_html=True)
+        elif custom_sentiment == "Positif":
+            st.markdown(f"Sentimen Ulasan: <span style='color:green; font-weight:bold;'>{custom_sentiment}</span>", unsafe_allow_html=True)
+        else:
+            st.write(f"Lengkapi Ulasan")
+    else:
+        st.write("Silahkan masukkan teks untuk dianalisis!")
+
+# Tambahkan credit di pojok kiri bawah
+st.markdown("""
+    <style>
+    .credit {
+        position: fixed;
+        bottom: 10px;
+        left: 10px;
+        font-size: 13px;
+        color: gray;
+    }
+    </style>
+    <div class="credit">Salsa Febriliana Sandita | 24050121130088 | 2025</div>
+""", unsafe_allow_html=True)
